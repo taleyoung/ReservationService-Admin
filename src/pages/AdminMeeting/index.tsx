c61f02281f6c73e0fed3ec4da99597ca559a7398
taleyoung <1090117853@qq.com>
2021-04-04 21:05:30 +0800

    feat: ❤️ add meeting query

--- src/pages/AdminMeeting/index.tsx
+++ src/pages/AdminMeeting/index.tsx
@@ -1,37 +1,75 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useRequest } from 'ice'
-import { Divider, Table, Pagination } from '@alifd/next'
+import { Divider, Table, Pagination, Form, Input, DatePicker, Button } from '@alifd/next'
 import meetingService from '@/service/room/meeting'
+
+const FormItem = Form.Item;
+const style = {
+    padding: '20px',
+    background: '#F7F8FA',
+    // margin: '20px'
+    width: '80%'
+};
+const formItemLayout = {
+    // labelWidth: 80,
+    colSpan: 3,
+};
 
 const tableColumn = [{
     title: '会议主题',
-    dataIndex: 'name'
+    dataIndex: 'name',
+    enableQuery: true
 }, {
     title: '会议日期',
-    dataIndex: 'date'
+    dataIndex: 'date',
+    enableQuery: true
 }, {
     title: '会议室',
-    dataIndex: 'meetingRoomId'
+    dataIndex: 'meetingRoomId',
+    enableQuery: true
 }, {
     title: '开始时间',
-    dataIndex: 'start'
+    dataIndex: 'start',
+    enableQuery: false
 }, {
     title: "结束时间",
-    dataIndex: 'end'
+    dataIndex: 'end',
+    enableQuery: false
 }, {
     title: "创建者",
-    dataIndex: 'creatorId'
+    dataIndex: 'creatorId',
+    enableQuery: true
 }, {
     title: "会议人数",
-    dataIndex: 'memberCount'
+    dataIndex: 'memberCount',
+    enableQuery: true
 }, {
     title: "创建时间",
-    dataIndex: 'createTime'
+    dataIndex: 'createTime',
+    enableQuery: false
 }]
+const defaultData = {
+    list: [],
+    totalCount: 1,
+    pageSize: 0
+}
 const AdminMeeting = () => {
-    const { data: roomInfoData = {}, loading, request } = useRequest(meetingService.getMeetingList, {
+    const { data, loading, request: getListReq } = useRequest(meetingService.getMeetingList, {
         manual: false
     });
+    const [meetingData, setMeetingData] = useState(defaultData);
+
+
+    useEffect(() => {
+        if (data) {
+            setMeetingData(data);
+        }
+    }, [data])
+
+    const formQuery = async (v) => {
+        const res = await getListReq(v);
+        setMeetingData(res);
+    }
 
     return <div>
         <div>
@@ -39,10 +77,29 @@ const AdminMeeting = () => {
             <Divider></Divider>
         </div>
         <div>
-            <Table dataSource={roomInfoData.list} loading={loading}>
+            <Form style={style} responsive>
+                {tableColumn.map(item => {
+                    if (item.enableQuery) {
+                        return <FormItem {...formItemLayout} label={item.title}>
+                            <Input placeholder="" id={item.dataIndex} name={item.dataIndex} />
+                        </FormItem>
+                    }
+                    return null;
+                })}
+                <FormItem colSpan={6} label='会议日期'>
+                    <DatePicker format="YYYY-M-D" id='date' name="date" />
+                </FormItem>
+                <FormItem wrapperCol={{ offset: 1 }} label="" colSpan={12} >
+                    <Form.Submit validate type="primary" onClick={(v) => formQuery(v)} style={{ marginRight: 10 }}>查询</Form.Submit>
+                    <Form.Reset type="secondary" onClick={() => formQuery({})} style={{ marginRight: 10 }}>重置</Form.Reset>
+                </FormItem>
+            </Form>
+        </div>
+        <div>
+            <Table dataSource={meetingData.list || []} loading={loading}>
                 {tableColumn.map(item => <Table.Column key={item.dataIndex} title={item.title} dataIndex={item.dataIndex} />)}
             </Table>
-            <Pagination total={roomInfoData.totalCount} pageSize={roomInfoData.pageSize} onChange={(curPage) => request(curPage)} />
+            <Pagination total={meetingData.totalCount} pageSize={meetingData.pageSize} onChange={(curPage) => getListReq(curPage)} />
         </div>
     </div>
 }
